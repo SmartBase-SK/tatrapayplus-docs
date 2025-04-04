580f1bb33b430aace24beba112fdbb6b05aea976
Viliam Mihalik <viliam.mihalik@smartbase.sk>
2025-04-04 12:27:22 +0200

    triv: changed domain to custom

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,10 +16,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://smartbase-sk.github.io",
+  url: "https://sdk.tatrabanka.sk",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/tatrapayplus-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

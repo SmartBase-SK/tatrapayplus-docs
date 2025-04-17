dc3523108a1d72f1d947c0769f60678e4b294830
lubos.zlatohlavek <lubos.zlatohlavek@smartbase.sk>
2025-04-17 10:32:10 +0200

    triv: #23799 added full payment to create payment code example

--- code_samples/python/v1.0.0/create_payment.py
+++ code_samples/python/v1.0.0/create_payment.py
@@ -10,12 +10,42 @@ client = TatrapayPlusClient(
 payment_data = InitiatePaymentRequest(
     base_payment=BasePayment(
         instructed_amount=Amount(
-            amount_value=120.0,
+            amount_value=10.0,
             currency="EUR",
         ),
         end_to_end="ORDER123456",
     ),
     bank_transfer=BankTransfer(),
+    pay_later=PayLater(
+        order=Order(
+            order_no="ORDER123456",
+            order_items=[
+                OrderItem(
+                    quantity=1,
+                    total_item_price=10.0,
+                    item_detail=ItemDetail(
+                        item_detail_sk=ItemDetailLangUnit(
+                            item_name="Testovací produkt",
+                            item_description="Popis produktu",
+                        ),
+                        item_detail_en=ItemDetailLangUnit(
+                            item_name="Test Product",
+                            item_description="Product description",
+                        ),
+                    ),
+                )
+            ],
+        ),
+    ),
+    card_detail=CardDetail(
+        card_holder="Janko Hruška",
+    ),
+    user_data=UserData(
+        first_name="Janko",
+        last_name="Hruska",
+        email="janko.hruska@example.com",
+    ),
 )
+
 
 response = client.create_payment(payment_data, "https://your-redirect-uri.com")

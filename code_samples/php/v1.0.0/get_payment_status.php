947d3052064a46b89d7fa9d12d915625f1d30032
Ľubomír Samotný <lubomir.samotny@smartbase.sk>
2025-05-09 15:00:14 +0200

    feat: update php docs after refactor

--- code_samples/php/v1.0.0/get_payment_status.php
+++ code_samples/php/v1.0.0/get_payment_status.php
@@ -7,10 +7,9 @@ $tatrapayplus_api = new TatraPayPlusAPIApi(
 );
 $payment_id = 'b54afd37-5bb9-4080-9416-5ec450779087'; // Retrieved from initiatePayment
 
-$response = $tatrapayplus_api->getPaymentIntentStatus($payment_id);
+[$simple_status, $response] = $tatrapayplus_api->getPaymentIntentStatus($payment_id);
 
 $response_obj = $response['object'];
-$simple_status = $response_obj->getSimpleStatus();
 $status = $response_obj->getStatus();
 $payment_method = $response_obj->getSelectedPaymentMethod();
 $authorization_status = $response_obj->getAuthorizationStatus();
